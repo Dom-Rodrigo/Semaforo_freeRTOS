84fc202441ca34ca4167e40122d958d12600995d
Dom-Rodrigo <rapiropo.cic@uesc.br>
2025-05-05 05:08:05 -0300

    Adiciona alguns beeps

--- blink.c
+++ blink.c
@@ -2,10 +2,14 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include <stdio.h>
+#include "hardware/pwm.h"
+#include "hardware/clocks.h"
+
 
 #define led_pin_green 11
-#define led_pin_blue 12
 #define led_pin_red 13
+#define BUZZER_A 21
+#define BUZZER_B 10
 
 void vBlinkTask()
 {
@@ -32,12 +36,50 @@ void vBlinkTask2()
     }
 }
 
+void vBeepVerde(){
+    while (true){
+        pwm_set_gpio_level(BUZZER_A, 2048);
+        vTaskDelay(pdMS_TO_TICKS(200));
+        pwm_set_gpio_level(BUZZER_A, 0); 
+        vTaskDelay(pdMS_TO_TICKS(2800));
+    }
+}
+
+void vBeepAmarelo(){
+    while (true){
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        pwm_set_gpio_level(BUZZER_A, 2048);
+        vTaskDelay(pdMS_TO_TICKS(200));
+        pwm_set_gpio_level(BUZZER_A, 0); 
+        vTaskDelay(pdMS_TO_TICKS(1800));
+    }
+}
+
+
 // Trecho para modo BOOTSEL com botão B
 #include "pico/bootrom.h"
 #define botaoB 6
 void gpio_irq_handler(uint gpio, uint32_t events)
 {
     reset_usb_boot(0, 0);
+}
+
+// Definição de uma função para inicializar o PWM no pino do buzzer
+void pwm_init_buzzer(uint pin, int frequency)
+{
+    // Configurar o pino como saída de PWM
+    gpio_set_function(pin, GPIO_FUNC_PWM);
+
+    // Obter o slice do PWM associado ao pino
+    uint slice_num = pwm_gpio_to_slice_num(pin);
+
+    // Configurar o PWM com frequência desejada
+    pwm_config config = pwm_get_default_config();
+    pwm_config_set_clkdiv(&config, clock_get_hz(clk_sys) / (frequency * 4096)); // Divisor de clock
+    pwm_init(slice_num, &config, true);
+
+    // Iniciar o PWM no nível baixo
+    pwm_set_gpio_level(pin, 0);
 }
 
 int main()
@@ -55,9 +97,22 @@ int main()
     gpio_init(led_pin_green);
     gpio_set_dir(led_pin_red, GPIO_OUT);
     gpio_set_dir(led_pin_green, GPIO_OUT);
-    xTaskCreate(vBlinkTask, "Blink Task", 
+
+    gpio_init(BUZZER_A);
+    gpio_set_dir(BUZZER_A, GPIO_OUT);
+    pwm_init_buzzer(BUZZER_A, 100);
+
+    gpio_init(BUZZER_B);
+    gpio_set_dir(BUZZER_B, GPIO_OUT);
+    pwm_init_buzzer(BUZZER_B, 100);
+
+    xTaskCreate(vBlinkTask, "Tarefa do LED VERDE", 
         configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL);
-    xTaskCreate(vBlinkTask2, "Blink Task 2", 
+    xTaskCreate(vBlinkTask2, "Tarefa do LED VERMELHO", 
+        configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL);
+    xTaskCreate(vBeepVerde, "Tareda do Beep do farol verde", 
+        configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL);
+    xTaskCreate(vBeepAmarelo, "Tareda do Beep do farol amarelo", 
         configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL);
     vTaskStartScheduler();
     panic_unsupported();
